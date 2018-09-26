1a4b4eea8db48c0700161a3b615ab5f4710cc70b
zjc0520 <1010266052@qq.com>
2018-09-26 16:33:25 +0800

    Add files via upload

---
+++ pandas-1.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+data=pd.read_csv("C:/Users/Administrator/Desktop/sndHsPr.csv")
+# print(data.head())
+print(data.count())#非na值的数量
+print(data.describe())
+data1=data["dist"]
+print(data[:,1])
+
+
+
+
